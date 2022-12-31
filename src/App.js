0e4a08d261dd7cfd768a9907f67ecda4978ba704
Siddhik234 <106161586+Siddhik234@users.noreply.github.com>
2022-12-31 10:23:23 +0530

    almost done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React,{useState,useEffect} from 'react';
+import React,{useState} from 'react';
 import Displayitem from './component/Displayitem';
 import Pagination from './component/Pagination';
 import data from "./component/data.json"
 import { TextField } from '@mui/material';
 
-
 function App() {
     const [currentPage, setCurrentPage] = useState(1);
     const [postsPerPage, setPostsPerPage] = useState(5);
-    
     
     const lastPostIndex = currentPage * postsPerPage;
     const firstPostIndex = lastPostIndex - postsPerPage;
--- src/component/Displayitem.jsx
+++ src/component/Displayitem.jsx
@@ -1,11 +1,17 @@
+import { TextField } from '@mui/material'
 import React from 'react'
 import "./display.css"
 import Displaycard from './Displaycard'
 
 const Displayitem = ({data}) => {
+
+
+
   return (
     
     <div className='div1'> 
+   
+
       <table>
         <tr>
         <th>
